d9165fc69e79aef8e3eed0f6c0f10aa289dc225e
Sejal Kumari <sejalkumari200231@gmail.com>
2021-10-18 18:25:59 +0530

    all search function

--- js/main.js
+++ js/main.js
@@ -1,34 +1,50 @@
 const baseUrl = "https://dummyapi.io/data/v1";
-const appKey = "613757bd2b9748529e4fa228";
+const appKey = "61658cba69b83795448792c2";
 
-function GetUsers() {
+function GetUsers(url) {
   var xHttp = new XMLHttpRequest();
   xHttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      var users = JSON.parse(this.responseText).data;
-
-      var divContent = document.getElementById("divContent");
-      for (var i = 0; i < users.length; i++) {
-        var div = document.createElement("div");
-        div.setAttribute("class", "contentPart1");
-        div.innerHTML = `<div class='rightPartLeft'><img src='${
-          users[i].picture
-        }'></div>
-                    <div class='rightPartRight'> ${users[i].id}
-                        <br /><span><b><a href='/profile.html?userid=${
-                          users[i].id
-                        }'>${
-          users[i].title + " " + users[i].firstName + " " + users[i].lastName
-        } </a></b></span>
-                    </div>
-                    <div class='clear'></div>`;
-        divContent.append(div);
+      var response = JSON.parse(this.responseText);
+      var users = response.data;
+      var limit = response.limit;
+      var total = response.total;
+      var numberOfButton = total / limit;
+      if (document.getElementsByClassName("contentHeading").length > 0) {
+        document.getElementsByClassName(
+          "contentHeading"
+        )[0].innerText = `Showing ${limit} of ${total} results...`;
       }
+
+      var divContentInner = document.getElementById("divContentInner");
+      var tempDiv = document.createElement("div");
+      for (var i = 0; i < users.length; i++) {
+        var div = getSingleUserBlock(users[i]);
+
+        tempDiv.append(div);
+      }
+      divContentInner.innerHTML = tempDiv.innerHTML;
+      buttonsForNewPagesUser(numberOfButton);
     }
   };
-  xHttp.open("GET", baseUrl + "/user", true);
+  xHttp.open("GET", baseUrl + url, true);
   xHttp.setRequestHeader("app-id", appKey);
   xHttp.send();
+}
+
+function getSingleUserBlock(user) {
+  var div = document.createElement("div");
+  div.setAttribute("class", "contentPart1");
+  div.innerHTML = `<div class='rightPartLeft'><img src='${user.picture}'></div>
+                    <div class='rightPartRight'> ${user.id}
+                        <br /><span><b><a href='/profile.html?userid=${
+                          user.id
+                        }'>${
+    user.title + " " + user.firstName + " " + user.lastName
+  } </a></b></span>
+                    </div>
+                    <div class='clear'></div>`;
+  return div;
 }
 
 function GetUserDetail() {
@@ -49,7 +65,12 @@ function GetUserDetail() {
 
       var leftImageName = document.getElementsByClassName("imageName")[0];
       var span = document.createElement("span");
-      span.innerText = userDetail.title + ". " +  userDetail.firstName +" "+ userDetail.lastName;
+      span.innerText =
+        userDetail.title +
+        ". " +
+        userDetail.firstName +
+        " " +
+        userDetail.lastName;
       leftImageName.append(span);
 
       //Details
@@ -89,48 +110,29 @@ function GetPosts(postUrl) {
 
   var xHttp = new XMLHttpRequest();
   xHttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {  
+    if (this.readyState == 4 && this.status == 200) {
       var response = JSON.parse(this.responseText);
       var limit = response.limit;
       var total = response.total;
-      if (document.getElementsByClassName("contentHeading").length>0) {
+      var numberOfButton = total / limit;
+      if (document.getElementsByClassName("contentHeading").length > 0) {
         document.getElementsByClassName(
           "contentHeading"
         )[0].innerText = `Showing ${limit} of ${total} results...`;
       }
-      
-     
-      console.log(response.data);
+
+      // console.log(response.data);
       var posts = response.data;
-      var divContent = document.getElementById("content");
+      var divContentInner = document.getElementById("divContentInner");
+      var tempDIV = document.createElement("div");
       for (var index = 0; index < posts.length; index++) {
         var post = posts[index];
-        console.log(post);
-        var div = document.createElement("div");
-        div.setAttribute("class", "contentPart1");
-        div.innerHTML = `<div class="contentTop"><img src="${post.owner.picture}" width="40" height="40"></div>
-                    <div class="contentTopRight"> <a href="/profile.html?userid=${post.owner.id}"> ${post.owner.title}. ${post.owner.firstName} ${post.owner.lastName}</a>
-                        <br />
-                      ${post.publishDate}
-                    </div>
-                    <div class="clear"></div>
-                    <div class="rightPartLeft"><img src="${post.image}" width="200"
-                            height="200"></div>
-                    <div class="rightPartRight"> <span class="cS">${post.publishDate}</span>
-                        <br /><span class="changeStyle">${post.text}</span><br /><br>
-                        <span class="pinkButton"><button>animals</button>
-                            <button>dog</button>
-                            <button>golden retriever</button></span>
-                        <br>
-                        <div class="like">
-                            <i class="far fa-thumbs-up"></i><span class="blue">&nbsp;&nbsp;&nbsp;${post.likes}</span>
-                        </div>
+        // console.log(post, "hello__________________________");
 
-                    </div>
-
-                    <div class="clear"></div>`;
-        divContent.append(div);
+        tempDIV.append(getSinglePostBlock(post));
       }
+      divContentInner.innerHTML = tempDIV.outerHTML;
+      buttonsForNewPages(numberOfButton);
     }
   };
 
@@ -139,34 +141,57 @@ function GetPosts(postUrl) {
   xHttp.send();
 }
 
+function getSinglePostBlock(post) {
+  var div = document.createElement("div");
+  div.setAttribute("class", "contentPart1");
+  div.innerHTML = `<div class="contentTop"><img src="${post.owner.picture}" width="40" height="40"></div>
+                    <div class="contentTopRight"> <a href="/profile.html?userid=${post.owner.id}"> ${post.owner.title}. ${post.owner.firstName} ${post.owner.lastName}</a>
+                    <br />
+                      ${post.publishDate}
+                      </div>
+                    <div class="clear"></div>
+                    <div class="rightPartLeft"><img src="${post.image}" width="200"
+                    height="200"></div>
+                    <div class="rightPartRight"> <span class="cS">${post.publishDate}</span>
+                    <br /><span class="changeStyle">${post.text}</span><br /><br>
+                    <span class="pinkButton"><button>animals</button>
+                            <button>dog</button>
+                            <button>golden retriever</button></span>
+                        <br>
+                        <div class="like">
+                        <i class="far fa-thumbs-up"></i><span class="blue">&nbsp;&nbsp;&nbsp;${post.likes}</span>
+                        </div>
+                        
+                        </div>
+                        
+                        <div class="clear"></div>`;
+  return div;
+}
+
 function GetUserFullDetails() {
   const urlParams = new URLSearchParams(window.location.search);
   const userId = urlParams.get("userid");
 
   var xHttp = new XMLHttpRequest();
   xHttp.onreadystatechange = function () {
-
     if (this.readyState == 4 && this.status == 200) {
       //call back method
       var userDetail = JSON.parse(this.responseText);
 
-
-
       var postByUserName = document.getElementsByClassName("postByUserName")[0];
       postByUserName.innerHTML = `<b class="heading">
-      Posts <span class="by">by
-      </span>${userDetail.firstName} 
-      ${userDetail.lastName}</b><br />`;
-
+                      Posts <span class="by">by
+                      </span>${userDetail.firstName} 
+                      ${userDetail.lastName}</b><br />`;
 
       var divContent = document.getElementById("topContent");
 
-      console.log(userDetail);
+      // console.log(userDetail, "-----------------------------------");
       var div = document.createElement("div");
       div.setAttribute("class", "divTopContentChild");
       div.innerHTML = ` 
                     <div class="topLeft">
-                        <div class="topLeft1">
+                    <div class="topLeft1">
                             <span class="tC1">${userDetail.title}. ${userDetail.firstName} ${userDetail.lastName}</span><br />
                             <span class="tC2">Gender:</span>
                             <span class="tC3">${userDetail.gender}</span>
@@ -222,35 +247,226 @@ function GetAllPosts() {
   GetPosts(postUrl);
 }
 
-
-
-
 function GetLatestPostsByUserId() {
   var xHttp = new XMLHttpRequest();
-xHttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    var response = JSON.parse(this.responseText);
-    console.log(response.data);
-    var posts = response.data;
+  xHttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var response = JSON.parse(this.responseText);
+      // console.log(response.data);
+      var posts = response.data;
 
-    var divLeft = document.getElementsByClassName("leftMiddle")[0];
-    for (var index = 0; index < posts.length; index++) {
-      var post = posts[index];
-      console.log(post);
-      var div = document.createElement("div");
-      div.setAttribute("class", "left1"); //<div class="left1"> </div>
+      var divLeft = document.getElementsByClassName("leftMiddle")[0];
+      for (var index = 0; index < posts.length; index++) {
+        var post = posts[index];
+        // console.log(post);
+        var div = document.createElement("div");
+        div.setAttribute("class", "left1"); //<div class="left1"> </div>
 
-      div.innerHTML = `<div class="leftPart1">${post.text}</div>
+        div.innerHTML = `<div class="leftPart1">${post.text}</div>
             <div class="leftPart2">
             ${post.owner.title}. ${post.owner.firstName} ${post.owner.lastName}<br/>
               <span> ${post.publishDate}</span>
             </div>`;
-      divLeft.append(div);
+        divLeft.append(div);
+      }
     }
-  }
-};
+  };
 
-  xHttp.open("GET", baseUrl + "/post?orderby=publishdate&page=0&limit=10", true);
+  xHttp.open(
+    "GET",
+    baseUrl + "/post?orderby=publishdate&page=0&limit=10",
+    true
+  );
   xHttp.setRequestHeader("app-id", appKey);
   xHttp.send();
+}
+
+function buttonsForNewPages(numberOfButton) {
+  var divContentPaging = document.getElementById("divContentPaging");
+  numberOfButton =
+    Math.round(numberOfButton) > numberOfButton
+      ? Math.round(numberOfButton)
+      : Math.round(numberOfButton) + 1;
+  var div = document.createElement("div");
+  div.setAttribute("class", "divClass");
+  for (let i = 1; i <= numberOfButton; i++) {
+    var button = document.createElement("button");
+    button.setAttribute("class", "buttonToChangePage");
+    button.innerText = i;
+    // button.style.width = "50%";
+
+    button.addEventListener("click", (event) => {
+      var pageNumber = event.target.innerText;
+      var postUrl = `/post?page=${pageNumber - 1}&limit=20`;
+      GetPosts(postUrl);
+    });
+
+    div.append(button);
+  }
+  if (divContentPaging.childNodes.length > 0) {
+    divContentPaging.removeChild(divContentPaging.childNodes[0]);
+  }
+  divContentPaging.append(div);
+}
+
+function buttonsForNewPagesUser(numberOfButton) {
+  var divContentPaging = document.getElementById("divContentPaging");
+  numberOfButton =
+    Math.round(numberOfButton) > numberOfButton
+      ? Math.round(numberOfButton)
+      : Math.round(numberOfButton) + 1;
+  var div = document.createElement("div");
+  div.setAttribute("class", "divClass");
+  for (let i = 1; i <= numberOfButton; i++) {
+    var button = document.createElement("button");
+    button.setAttribute("class", "buttonToChangePage");
+    button.innerText = i;
+    // button.style.width = "50%";
+
+    button.addEventListener("click", (event) => {
+      var pageNumber = event.target.innerText;
+      var UserUrl = `/user?page=${pageNumber - 1}&limit=20`;
+      GetUsers(UserUrl);
+    });
+
+    div.append(button);
+  }
+  if (divContentPaging.childNodes.length > 0) {
+    divContentPaging.removeChild(divContentPaging.childNodes[0]);
+  }
+  divContentPaging.append(div);
+}
+
+var userArray = [];
+var postsArray = [];
+function searchUser(url) {
+  // console.log(url);
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", baseUrl + url, true);
+  xhr.setRequestHeader("app-id", appKey);
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // console.log(this.responseText);
+      var response = JSON.parse(xhr.responseText);
+      var page = response.page;
+      if (response.data != null && response.data.length > 0) {
+        // console.log("r----------",page);
+        userArray = userArray.concat(response.data);
+        searchUser(`/user?page=${page + 1}&limit=50&created=0`);
+      } else {
+        console.log(
+          userArray.length,
+          userArray,
+          "---------------------userarray---------------"
+        );
+      }
+    }
+  };
+  xhr.onerror = function () {
+    // console.log(this.responseText);
+  };
+  xhr.send();
+}
+setTimeout(function () {
+  searchUser("/user?page=0&limit=50&created=0");
+  searchPost("/post?page=0&limit=50&created=0");
+}, 5000);
+
+function searchPost(postUrl) {
+  // console.log(postUrl);
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", baseUrl + postUrl, true);
+  xhr.setRequestHeader("app-id", appKey);
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // console.log(this.responseText);
+      var response = JSON.parse(xhr.responseText);
+      var page = response.page;
+      if (response.data != null && response.data.length > 0) {
+        // console.log("r----------rrrrr", page);
+        postsArray = postsArray.concat(response.data);
+        searchPost(`/post?page=${page + 1}&limit=50&created=0`);
+      } else {
+        console.log(
+          postsArray.length,
+          postsArray,
+          "---------------------postsArray---------------"
+        );
+      }
+    }
+  };
+  xhr.onerror = function () {
+    console.log(this.responseText);
+  };
+  xhr.send();
+}
+
+function searchFun() {
+  const urlParams = new URLSearchParams(window.location.search); //querry string 
+  const searchParams = urlParams.get("search");
+  console.log(searchParams);
+
+  document.getElementsByClassName("box")[0].value = searchParams;
+  setTimeout(function () {
+    const filteredUserArray = userArray.filter(
+      (singleUserData) =>
+        singleUserData.firstName
+          .toLowerCase()
+          .includes(searchParams.toLowerCase()) ||
+        singleUserData.lastName
+          .toLowerCase()
+          .includes(searchParams.toLowerCase()) ||
+        singleUserData.id == searchParams
+    );
+    console.log(filteredUserArray);
+
+    const filteredPostArray = postsArray.filter(
+      ({ id, tags, text, owner }) =>
+        id == searchParams ||
+        tags.includes(searchParams.toLowerCase()) ||
+        text.toLowerCase().includes(searchParams.toLowerCase()) ||
+        owner.firstName.toLowerCase().includes(searchParams.toLowerCase()) ||
+        owner.lastName.toLowerCase().includes(searchParams.toLowerCase()) ||
+        owner.id == searchParams
+    );
+    console.log(filteredPostArray);
+    populateSearchedData(filteredUserArray, filteredPostArray);
+  }, 50000);
+}
+
+document
+  .getElementsByClassName("search")[0]
+  .addEventListener("click", function () {
+    var ab = document.getElementsByClassName("box")[0].value;
+    if (ab.trim() == "") {
+      alert("Enter some data");
+    } else {
+      location.replace(`/search.html?search=${ab}`);
+    }
+  });
+
+function populateSearchedData(filteredUserArray, filteredPostArray) {
+  document.getElementsByClassName("contentHeading")[0].innerText = `Found ${
+    filteredUserArray.length == 0
+      ? ""
+      : filteredUserArray.length == 1
+      ? filteredUserArray.length + " User"
+      : filteredUserArray.length + " Users"
+  }  ${filteredUserArray.length > 0 && filteredPostArray > 0 ? "and" : ""} ${
+    filteredPostArray.length == 0
+      ? ""
+      : filteredPostArray.length == 1
+      ? filteredPostArray.length + " Post"
+      : filteredPostArray.length + " Posts"
+  }`;
+  filteredUserArray.forEach((element) => {
+    document
+      .getElementById("divContentUser")
+      .append(getSingleUserBlock(element));
+  });
+  filteredPostArray.forEach((element) => {
+    document
+      .getElementById("divContentPost")
+      .append(getSinglePostBlock(element));
+  });
 }
